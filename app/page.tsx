718a4747a2ea09535fa93274ffbcbec2c3f0f49b
vikaslime <viktorya2008.82@gmail.com>
2026-06-10 13:37:07 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main>
+      <section className="hero">
+        <h1>WOW SLIME LAB</h1>
+
+        <h2>Создай 3 невероятных слайма за 3 дня!</h2>
+
+        <p>
+          Весёлый онлайн-челлендж для детей 7–12 лет.
+        </p>
+
+        <button>🚀 ХОЧУ УЧАСТВОВАТЬ</button>
+      </section>
+    </main>
+  );
+}
